a362da4748f2231c6274b864b3df2153aaa51089
Yan Sandalov <isandalov@mail.ru>
2022-09-05 07:50:54 +0300

    module 8-1

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,21 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render()
   }
+
+  render() {
+    this.elem = createElement(`
+    <div class="products-grid">
+    <div class="products-grid__inner">
+    </div>
+    </div>
+    `)
+
+    for (let key of ProductCard) {
+      this.elem.queueSelector(".products-grid__inner").append(key)
+    }
+  }
+
+
 }
